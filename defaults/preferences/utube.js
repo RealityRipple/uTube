425414f16710d608dc78efc29aab80e0a3b39a65
Andy <webmaster@RealityRipple.com>
2022-03-19 19:54:13 -0700

    No More Legacy
    Pale Moon v30 is now the only officially supported product.

--- defaults/preferences/utube.js
+++ defaults/preferences/utube.js
@@ -1,4 +1,4 @@
 pref("extensions.{EC845603-2E80-5C58-AEB0-7791201C9A9C}.description", "chrome://utube/locale/utube.properties");
 pref("extensions.utube.autoplay", false);
 pref("extensions.utube.hosted", true);
-pref("extensions.utube.hostedURL", "https://realityripple.com/Software/Mozilla-Extensions/uTube/play.html");
+pref("extensions.utube.hostedURL", "https://realityripple.com/Software/XUL/uTube/play.html");
